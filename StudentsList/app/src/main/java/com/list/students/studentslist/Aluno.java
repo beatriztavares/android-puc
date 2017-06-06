2a8edfa40315251a86d41836cf1ae8934ec4cd1f
Beatriz Tavares <beatriztavares@gmail.com>
2017-06-05 23:30:44 -0300

    iniciando o projeto

---
+++ StudentsList/app/src/main/java/com/list/students/studentslist/Aluno.java
@@ -0,0 +1,58 @@
+package com.list.students.studentslist;
+
+import java.util.Date;
+
+public class Aluno {
+
+    private String objectId, fotoUrl, nome, telefone, endereco;
+    private int idade;
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
