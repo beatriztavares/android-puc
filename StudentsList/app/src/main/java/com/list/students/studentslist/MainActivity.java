081dd62fd34559fde524e06301b9ed92e08153f5
Beatriz Tavares <beatriztavares@gmail.com>
2017-08-01 22:08:42 -0300

    some design modifications

--- StudentsList/app/src/main/java/com/list/students/studentslist/MainActivity.java
+++ StudentsList/app/src/main/java/com/list/students/studentslist/MainActivity.java
@@ -3,14 +3,19 @@ package com.list.students.studentslist;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -40,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     View view, viewList;
     public final static String EXTRA_ENDERECO = "ENDERECO";
     public final static String EXTRA_NOME = "NOME";
+    MenuItem day, night;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         viewList = inflater.inflate(R.layout.activity_list, null);
         setContentView(viewList);
         view = inflater.inflate(R.layout.dialog_add, null);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("StudentsList");
 
         final EditText url = (EditText)view.findViewById(R.id.url);
         foto = (ImageView)view.findViewById(R.id.fotoAluno);
@@ -91,6 +102,37 @@ public class MainActivity extends AppCompatActivity {
 
         callGetAlunos();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        day = menu.findItem(R.id.menu_day);
+        night = menu.findItem(R.id.menu_nigth);
+        day.setVisible(false);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_day:
+                getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                toolbar.setBackgroundColor(Color.parseColor("#3fa7b5"));
+                day.setVisible(false);
+                night.setVisible(true);
+                return true;
+            case R.id.menu_nigth:
+                getWindow().getDecorView().setBackgroundColor(Color.DKGRAY);
+                toolbar.setBackgroundColor(Color.parseColor("#303F9F"));
+                day.setVisible(true);
+                night.setVisible(false);
+                return true;
+            case R.id.menu_out:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     private void atualizaFoto(String url) {
         Picasso.with(this)
@@ -107,17 +149,22 @@ public class MainActivity extends AppCompatActivity {
         final EditText fotoUrl = (EditText)view.findViewById(R.id.url);
         pDialog.setMessage("Adicionando aluno(a)...");
 
-        AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setView(view)
         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 Aluno novo = new Aluno();
-                novo.setNome(name.getText().toString());
-                novo.setIdade(Integer.getInteger(idade.getText().toString()));
-                novo.setTelefone(telefone.getText().toString());
-                novo.setEndereco(endereco.getText().toString());
-                novo.setFotoUrl(fotoUrl.getText().toString());
-                callAddAluno(novo);
+                if((novo.getNome() != null) && (idade.getText().toString() != null) &&
+                        (telefone.getText().toString() != null) &&
+                        (endereco.getText().toString() != null) &&
+                        (fotoUrl.getText().toString() == null)){
+                    novo.setNome(name.getText().toString());
+                    novo.setIdade(Integer.parseInt(idade.getText().toString()));
+                    novo.setTelefone(telefone.getText().toString());
+                    novo.setEndereco(endereco.getText().toString());
+                    novo.setFotoUrl(fotoUrl.getText().toString());
+                    callAddAluno(novo);
+                }
             }
         })
         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -194,9 +241,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showMaps(Aluno aluno) {
+        pDialog.setMessage("Buscando endereço...");
+        pDialog.setIndeterminate(true);
+        pDialog.setCancelable(false);
+        pDialog.show();
         Intent intent = new Intent(this, MapsActivity.class);
         intent.putExtra(EXTRA_ENDERECO, aluno.getEndereco());
         intent.putExtra(EXTRA_NOME, aluno.getNome());
+        pDialog.dismiss();
         startActivity(intent);
     }
 
